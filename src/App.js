ad12e045c640f88cdc01805f79dea5425d68648d
rakib ahmed <rakibahmed.dev@gmail.com>
2022-04-20 15:52:00 +0600

    dynamic route useing useParams

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import About from './Pages/About/About';
-import Home from './Pages/Home/Home/Home';
-import Footer from './Pages/Shared/Footer/Footer';
-import Header from './Pages/Shared/Header/Header';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./Pages/About/About";
+import Home from "./Pages/Home/Home/Home";
+import ServiceDetail from "./Pages/ServiceDetail/ServiceDetail";
+import Footer from "./Pages/Shared/Footer/Footer";
+import Header from "./Pages/Shared/Header/Header";
 
 function App() {
   return (
@@ -12,6 +13,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/about" element={<About></About>}></Route>
+        <Route
+          path="/service/:serviceId"
+          element={<ServiceDetail></ServiceDetail>}
+        ></Route>
       </Routes>
       <Footer></Footer>
     </div>
